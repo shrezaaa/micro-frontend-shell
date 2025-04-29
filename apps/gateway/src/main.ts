d45a4fe2fd4f7d1ed8c8285d34a39cde1b2de5d6
Reza Shakeri <53859479+shrezaaa@users.noreply.github.com>
2025-04-30 00:35:54 +0330

    feat: add docker

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -14,11 +14,18 @@ const PORT = 9000;
 
 // Map each app to its static path or dev URL:
 const apps = {
-  "shell":       { staticDir: path.join(__dirname, "../dist/shell"),       devUrl: "http://host.docker.internal:5000" },
-  "angular-app": { staticDir: path.join(__dirname, "../dist/angular-app"), devUrl: "http://host.docker.internal:5001" },
-  "react-app":   { staticDir: path.join(__dirname, "../dist/react-app"),   devUrl: "http://host.docker.internal:5002" },
-  "vue-app":     { staticDir: path.join(__dirname, "../dist/vue-app"),     devUrl: "http://host.docker.internal:5003" },
+  "shell":       { staticDir: path.join(__dirname, "../shell/browser"),       devUrl: "http://localhost:5000" },
+  "angular-app": { staticDir: path.join(__dirname, "../angular-app/browser"), devUrl: "http://localhost:5001" },
+  "react-app":   { staticDir: path.join(__dirname, "../react-app"),   devUrl: "http://localhost:5002" },
+  "vue-app":     { staticDir: path.join(__dirname, "../vue-app"),     devUrl: "http://localhost:5003" },
 };
+
+// const apps = {
+//   "shell":       { staticDir: path.join(__dirname, "../shell/browser"),       devUrl: "http://host.docker.internal:5000" },
+//   "angular-app": { staticDir: path.join(__dirname, "../angular-app/browser"), devUrl: "http://host.docker.internal:5001" },
+//   "react-app":   { staticDir: path.join(__dirname, "../react-app"),   devUrl: "http://host.docker.internal:5002" },
+//   "vue-app":     { staticDir: path.join(__dirname, "../vue-app"),     devUrl: "http://host.docker.internal:5003" },
+// };
 
 Object.keys(apps).forEach((name) => {
   const route = `/${name}`;
